e76be6b81294d9c6706f7459cc073841d4d1a05f
Alp Erkent <alp.erkent@gmail.com>
2024-05-24 17:03:15 -0400

    add tests for cli.tools, update bids-examples

--- tests/cli/tools/test_bids_parser.py
+++ tests/cli/tools/test_bids_parser.py
@@ -1,7 +1,4 @@
-"""Tests for eeg_research.cli.toolbox.bids_parser.py.
-
-TODO: Add tests for _set_layout, _set_entities and update_layout.
-"""
+"""Tests for eeg_research.cli.toolbox.bids_parser.py."""
 
 import sys
 from pathlib import Path
@@ -12,35 +9,17 @@ from pyparsing import Any
 import eeg_research.cli.tools.bids_parser as script
 
 
-def test_bids_parser_init(mocker: Any) -> None:
-    """Test the initialization of the BIDSParser class."""
-    mock_parse_arguments = mocker.patch.object(script.BIDSParser, "_parse_arguments")
-    mock_set_reading_root = mocker.patch.object(script.BIDSParser, "_set_reading_root")
-    mock_set_layout = mocker.patch.object(script.BIDSParser, "_set_layout")
-    mock_set_entities = mocker.patch.object(script.BIDSParser, "_set_entities")
-
-    _ = script.BIDSParser()
-
-    assert mock_parse_arguments.called
-    assert mock_set_reading_root.called
-    assert mock_set_layout.called
-    assert mock_set_entities.called
-
-
-def test_bids_parser_set_reading_root(mocker: Any) -> None:
-    """Test the _set_reading_root method of the BIDSParser class."""
+@pytest.fixture
+def mock_parser(mocker: Any, tmp_path: Path) -> script.BIDSParser:
+    """Fixture to create a mock BIDSLayout."""
     args = mocker.MagicMock()
-    args.root = "bids-examples/eeg_ds000117"
-    args.datafolder = ""
-
+    args.root = tmp_path
+    args.datafolder = None
     mocker.patch.object(script.BIDSParser, "_parse_arguments", return_value=args)
     mocker.patch("bids.BIDSLayout", return_value=mocker.MagicMock())
-
     parser = script.BIDSParser()
-
-    result = parser._set_reading_root()
-
-    assert result == Path("bids-examples/eeg_ds000117/")
+    parser.layout = mocker.MagicMock()
+    return parser
 
 
 def run_bids_parser_parse_arguments_test(argv: list[str], expected: dict) -> None:
@@ -67,90 +46,121 @@ def run_test(option: str, value: str, expected: dict) -> None:
     )
 
 
-def test_bids_parser_parse_arguments_root_required() -> None:
-    """Test that the root argument is required."""
+def test_bids_parser_init(mocker: Any) -> None:
+    """Test the initialization of the BIDSParser class."""
+    mock_parse_arguments = mocker.patch.object(script.BIDSParser, "_parse_arguments")
+    mock_set_reading_root = mocker.patch.object(script.BIDSParser, "_set_reading_root")
+    mock_set_layout = mocker.patch.object(script.BIDSParser, "_set_layout")
+    mock_set_entities = mocker.patch.object(script.BIDSParser, "_set_entities")
+
+    _ = script.BIDSParser()
+
+    assert mock_parse_arguments.called
+    assert mock_set_reading_root.called
+    assert mock_set_layout.called
+    assert mock_set_entities.called
+
+
+def test_bids_parser_set_reading_root(mock_parser: script.BIDSParser) -> None:
+    """Test the _set_reading_root method of the BIDSParser class."""
+    mock_parser.args.root = "mock_root"
+    mock_parser.args.datafolder = None
+    result = mock_parser._set_reading_root()
+    assert result == Path("mock_root")
+
+    mock_parser.args.root = "mock_root"
+    mock_parser.args.datafolder = "test"
+    result = mock_parser._set_reading_root()
+    assert result == Path("mock_root/test")
+
+
+@pytest.mark.parametrize(
+    "argv",
+    [
+        ["prog"],
+        ["prog", "--root", "bids-examples/eeg_ds000117"],
+    ],
+)
+def test_bids_parser_parse_arguments_required_flags(argv: list[str]) -> None:
+    """Test that the error is raised when the required flags are not provided."""
     with pytest.raises(SystemExit):
-        run_bids_parser_parse_arguments_test(["prog"], {})
+        run_bids_parser_parse_arguments_test(argv, {})
 
 
-def test_bids_parser_parse_arguments_default_values() -> None:
-    """Test that the default values are set correctly."""
-    run_bids_parser_parse_arguments_test(
-        ["prog", "--root", "bids-examples/eeg_ds000117", "--gradient"],
-        {
-            "root": "bids-examples/eeg_ds000117",
-            "datafolder": None,
-            "subject": None,
-            "session": None,
-            "run": None,
-            "task": None,
-            "extension": None,
-            "datatype": "eeg",
-            "suffix": "eeg",
-            "description": None,
-            "interactive": False,
-            "gradient": True,
-            "bcg": False,
-            "qc": False,
-        },
-    )
-
-
-def test_bids_parser_parse_arguments_all_values() -> None:
-    """Test that all values are parsed correctly."""
-    run_bids_parser_parse_arguments_test(
-        [
-            "prog",
-            "--root",
-            "bids-examples/eeg_rest_fmri",
-            "--datafolder",
-            "derivatives",
-            "--subject",
-            "1",
-            "--session",
-            "1",
-            "--run",
-            "1",
-            "--task",
-            "a",
-            "--extension",
-            "a",
-            "--datatype",
-            "a",
-            "--suffix",
-            "a",
-            "--description",
-            "desc",
-            "--interactive",
-            "--gradient",
-            "--bcg",
-            "--qc",
-        ],
-        {
-            "root": "bids-examples/eeg_rest_fmri",
-            "datafolder": "derivatives",
-            "subject": "1",
-            "session": "1",
-            "run": "1",
-            "task": "a",
-            "extension": "a",
-            "datatype": "a",
-            "suffix": "a",
-            "description": "desc",
-            "interactive": True,
-            "gradient": True,
-            "bcg": True,
-            "qc": True,
-        },
-    )
-
-
-def test_bids_parser_parse_arguments_no_flags() -> None:
-    """Test case for the parse_arguments function when no flags are provided."""
-    with pytest.raises(SystemExit):
-        run_bids_parser_parse_arguments_test(
-            ["prog", "--root", "bids-examples/eeg_ds000117"], {}
-        )
+@pytest.mark.parametrize(
+    "argv, expected",
+    [
+        (
+            ["prog", "--root", "bids-examples/eeg_ds000117", "--gradient"],
+            {
+                "root": "bids-examples/eeg_ds000117",
+                "datafolder": None,
+                "subject": None,
+                "session": None,
+                "run": None,
+                "task": None,
+                "extension": None,
+                "datatype": "eeg",
+                "suffix": "eeg",
+                "description": None,
+                "interactive": False,
+                "gradient": True,
+                "bcg": False,
+                "qc": False,
+            },
+        ),
+        (
+            [
+                "prog",
+                "--root",
+                "bids-examples/eeg_rest_fmri",
+                "--datafolder",
+                "derivatives",
+                "--subject",
+                "1",
+                "--session",
+                "1",
+                "--run",
+                "1",
+                "--task",
+                "a",
+                "--extension",
+                "a",
+                "--datatype",
+                "a",
+                "--suffix",
+                "a",
+                "--description",
+                "desc",
+                "--interactive",
+                "--gradient",
+                "--bcg",
+                "--qc",
+            ],
+            {
+                "root": "bids-examples/eeg_rest_fmri",
+                "datafolder": "derivatives",
+                "subject": "1",
+                "session": "1",
+                "run": "1",
+                "task": "a",
+                "extension": "a",
+                "datatype": "a",
+                "suffix": "a",
+                "description": "desc",
+                "interactive": True,
+                "gradient": True,
+                "bcg": True,
+                "qc": True,
+            },
+        ),
+    ],
+)
+def test_bids_parser_parse_arguments_default_and_all_values(
+    argv: list[str], expected: dict
+) -> None:
+    """Test that the default and all values are parsed correctly."""
+    run_bids_parser_parse_arguments_test(argv, expected)
 
 
 def test_bids_parser_parse_arguments_options() -> None:
@@ -170,83 +180,108 @@ def test_bids_parser_parse_arguments_options() -> None:
             run_test(option, "eeg", {option: "eeg"})
 
 
-def test_bids_parser_parse_arguments_description() -> None:
-    """Test that the description is parsed correctly."""
-    run_bids_parser_parse_arguments_test(
-        [
-            "prog",
-            "--root",
-            "bids-examples/eeg_ds000117",
-            "--description",
-            "desc",
-            "--gradient",
-        ],
-        {"description": "desc"},
+@pytest.mark.parametrize(
+    "return_value, arg, expected",
+    [
+        (["1", "2", "3"], "*", ["1", "2", "3"]),
+        (["1", "2", "3", "4", "5"], "2-4", ["2", "3", "4"]),
+        (None, "value", "value"),
+    ],
+)
+def test_bids_parser_parse_range_args_valid(
+    mock_parser: script.BIDSParser,
+    return_value: list[str] | None,
+    arg: str,
+    expected: list[str] | str,
+) -> None:
+    """Test parse_range_arg function with valid values."""
+    mock_parser.layout.get.return_value = return_value
+    assert mock_parser._parse_range_args("entity", arg) == expected
+
+
+@pytest.mark.parametrize(
+    "return_value, arg",
+    [
+        (["1", "2", "3", "4", "5"], "5-2"),
+        (["a", "b", "c"], "1-2"),
+        (["1", "2", "3", "4", "5"], "6-7"),
+        (["1", "2", "3", "4", "5"], "-6"),
+        (["1", "2", "3", "4", "5"], "6-"),
+    ],
+)
+def test_bids_parser_parse_range_args_invalid(
+    mock_parser: script.BIDSParser, return_value: list[str], arg: str
+) -> None:
+    """Test parse_range_arg function with invalid values."""
+    mock_parser.layout.get.return_value = return_value
+    with pytest.raises(ValueError):
+        mock_parser._parse_range_args("entity", arg)
+
+
+def test_bids_parser_update_layout(mock_parser: script.BIDSParser, mocker: Any) -> None:
+    """Test the update_layout method of the BIDSParser class."""
+    mock_indexer = mocker.patch(
+        "bids.BIDSLayoutIndexer", return_value=mocker.MagicMock()
     )
 
+    mock_layout = mocker.patch.object(mock_parser, "layout")
+    mock_layout.get.side_effect = [["file1", "file2", "file3"], ["file2", "file3"]]
 
-def test_bids_parser_parse_arguments_flags() -> None:
-    """Test that the flags are parsed correctly."""
-    flags = ["interactive", "gradient", "bcg", "qc"]
-    for flag in flags:
-        run_bids_parser_parse_arguments_test(
-            ["prog", "--root", "bids-examples/eeg_ds000117", f"--{flag}"],
-            {flag: True},
-        )
+    entities: dict[str, str | None] = {"subject": "1", "session": "1", "run": "1"}
 
+    updated_layout = mock_parser.update_layout(entities)
 
-@pytest.fixture
-def mock_parser(mocker: Any) -> script.BIDSParser:
-    """Fixture to create a mock BIDSLayout."""
-    args = mocker.MagicMock()
-    args.root = "bids-examples/eeg_ds000117"
-    args.datafolder = ""
-    mocker.patch.object(script.BIDSParser, "_parse_arguments", return_value=args)
-    mocker.patch("bids.BIDSLayout", return_value=mocker.MagicMock())
-    parser = script.BIDSParser()
-    parser.layout = mocker.MagicMock()
-    return parser
+    mock_layout.get.assert_called_with(return_type="file", **entities)
+    assert isinstance(updated_layout, mocker.MagicMock)
+    mock_indexer.assert_called_with(ignore=mocker.ANY)
+    ignore_arg = mock_indexer.call_args[1]["ignore"]
+    assert "file1" in ignore_arg
+    assert "file2" and "file3" not in ignore_arg
 
 
-def test_bids_parser_parse_range_args_wildcard(mock_parser: script.BIDSParser) -> None:
-    """Test parse_range_arg function with wildcard value."""
-    mock_parser.layout.get.return_value = ["1", "2", "3"]
-    assert mock_parser._parse_range_args("entity", "*") == ["1", "2", "3"]
+def test_bids_parser_set_layout(mock_parser: script.BIDSParser, mocker: Any) -> None:
+    """Test the _set_layout method of the BIDSParser class."""
+    mock_indexer = mocker.MagicMock()
+    mock_layout = mocker.patch("bids.BIDSLayout", return_value=mocker.MagicMock())
+
+    # Test when args.datafolder is None or doesn't contain "derivatives"
+    result = mock_parser._set_layout(mock_indexer)
+    mock_layout.assert_called_with(root=mock_parser.reading_root, indexer=mock_indexer)
+    assert result == mock_layout.return_value
+
+    # Test when args.datafolder contains "derivatives"
+    mock_parser.args.datafolder = "derivatives"
+    result = mock_parser._set_layout(mock_indexer)
+    mock_layout.assert_called_with(
+        root=mock_parser.reading_root,
+        validate=False,
+        is_derivative=True,
+        indexer=mock_indexer,
+    )
+    assert result == mock_layout.return_value
 
 
-def test_bids_parser_parse_range_args_range(mock_parser: script.BIDSParser) -> None:
-    """Test parse_range_arg function with range value."""
-    mock_parser.layout.get.return_value = ["1", "2", "3", "4", "5"]
-    assert mock_parser._parse_range_args("entity", "2-4") == ["2", "3", "4"]
+def test_bids_parser_set_entities(mock_parser: script.BIDSParser, mocker: Any) -> None:
+    """Test the _set_entities method of the BIDSParser class."""
+    mock_parse_range_args = mocker.patch.object(
+        mock_parser, "_parse_range_args", return_value=mocker.MagicMock()
+    )
 
+    result = mock_parser._set_entities()
+    result_description = result.pop("description")
 
-def test_bids_parser_parse_range_args_non_range(mock_parser: script.BIDSParser) -> None:
-    """Test parse_range_arg function with non-range value."""
-    assert mock_parser._parse_range_args("entity", "value") == "value"
+    entity_names = [
+        "subject",
+        "session",
+        "run",
+        "task",
+        "extension",
+        "datatype",
+        "suffix",
+    ]
 
+    for name in entity_names:
+        mock_parse_range_args.assert_any_call(name, getattr(mock_parser.args, name))
 
-def test_bids_parser_parse_range_args_invalid_range(
-    mock_parser: script.BIDSParser,
-) -> None:
-    """Test parse_range_arg function with invalid range value."""
-    mock_parser.layout.get.return_value = ["1", "2", "3", "4", "5"]
-    with pytest.raises(ValueError):
-        mock_parser._parse_range_args("entity", "5-2")
-
-
-def test_bids_parser_parse_range_args_non_integer_ids(
-    mock_parser: script.BIDSParser,
-) -> None:
-    """Test parse_range_arg function with non-integer ids."""
-    mock_parser.layout.get.return_value = ["a", "b", "c"]
-    with pytest.raises(ValueError):
-        mock_parser._parse_range_args("entity", "1-2")
-
-
-def test_bids_parser_parse_range_args_no_ids_in_range(
-    mock_parser: script.BIDSParser,
-) -> None:
-    """Test parse_range_arg function with no ids in range."""
-    mock_parser.layout.get.return_value = ["1", "2", "3", "4", "5"]
-    with pytest.raises(ValueError):
-        mock_parser._parse_range_args("entity", "6-7")
+    assert result == {name: mock_parse_range_args.return_value for name in entity_names}
+    assert result_description == mock_parser.args.description
--- tests/cli/tools/test_interactive_menu.py
+++ tests/cli/tools/test_interactive_menu.py
@@ -1,10 +1,65 @@
-# ruff: noqa
-# remove the first line after populating the file
+"""Tests for eeg_research.cli.toolbox.interactive_menu.py."""
 
 import pytest
+from pyparsing import Any
 
 import eeg_research.cli.tools.interactive_menu as script
 
 
-def test_example():
-    assert True
+@pytest.fixture
+def menu(mocker: Any, request: pytest.FixtureRequest) -> script.InteractiveMenu:
+    """Fixture to create an InteractiveMenu object."""
+    mocker.patch("eeg_research.cli.tools.interactive_menu.TerminalMenu")
+    menu_entries = request.param[0]
+    entity = request.param[1]
+    preselection = request.param[2]
+    return script.InteractiveMenu(menu_entries, entity, "title", preselection)
+
+
+@pytest.mark.parametrize("menu", [(["item1"], "description", [0])], indirect=True)
+def test_interactive_menu_init(menu: script.InteractiveMenu) -> None:
+    """Test the initialization of the InteractiveMenu class."""
+    assert menu.menu_entries == ["item1"]
+    assert menu.entity == "description"
+    assert menu.title == "title"
+    assert menu.preselection == [0]
+
+
+@pytest.mark.parametrize("menu", [(["item1", "item2"], "task", [0])], indirect=True)
+def test_create_menu(menu: script.InteractiveMenu) -> None:
+    """Test the _create_menu method of the InteractiveMenu class."""
+    assert menu._create_menu()
+
+
+@pytest.mark.parametrize("menu", [(["item1", "item2"], "suffix", [0])], indirect=True)
+def test_handle_user_input_select_all(menu: script.InteractiveMenu) -> None:
+    """Test the _handle_user_input method when all items are selected."""
+    menu.selected_indices = [0]
+    assert menu._handle_user_input() == ["item1", "item2"]
+
+
+@pytest.mark.parametrize(
+    "menu", [(["1", "2", "3", "4"], "subject", None)], indirect=True
+)
+def test_handle_user_input_enter_range(
+    mocker: Any, menu: script.InteractiveMenu
+) -> None:
+    """Test the _handle_user_input method when a range is entered."""
+    mocker.patch("builtins.input", side_effect=["2", "4"])
+    menu.selected_indices = [1]
+    assert menu._handle_user_input() == ["2", "3", "4"]
+
+
+@pytest.mark.parametrize("menu", [(["1", "2", "3"], "session", [2, 3])], indirect=True)
+def test_handle_user_input_specific_items(menu: script.InteractiveMenu) -> None:
+    """Test the _handle_user_input method when specific items are selected."""
+    menu.selected_indices = [2, 3]
+    assert menu._handle_user_input() == ["1", "2"]
+
+
+@pytest.mark.parametrize("menu", [(["1", "2", "3"], "session", [2, 3])], indirect=True)
+def test_get_selected_items(menu: script.InteractiveMenu) -> None:
+    """Test the get_selected_items method of the InteractiveMenu class."""
+    menu.selected_indices = [2, 3]
+    menu.selected_items = menu._handle_user_input()
+    assert menu.get_selected_items() == ["1", "2"]
